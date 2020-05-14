c407b4a50c15ab05cc58f97f73d9d22fd461adbb
tiduszhang <tiduszhang@vip.qq.com>
2020-05-14 19:19:37 +0800

    Win32API，ApplicationTools

---
+++ WPF-Core-Framework/Common/ApplicationTools.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Common
+{
+    /// <summary>
+    /// 应用程序帮助类
+    /// </summary>
+    public static class ApplicationTools
+    {
+
+        /// <summary>
+        /// 只打开一个进程
+        /// </summary>
+        public static System.Threading.EventWaitHandle StartOnece(string mutexName)
+        {
+            System.Threading.EventWaitHandle ProgramStarted = new System.Threading.EventWaitHandle(false, System.Threading.EventResetMode.AutoReset, mutexName, out var createNew);
+
+            if (!createNew)
+            {
+                try
+                {
+                    var processes = System.Diagnostics.Process.GetProcessesByName(WorkPath.AssemblyName);
+                    if (!processes.Any())
+                    {
+                        //MessageBox.Show("程序已启动。", "提示提示", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        foreach (System.Diagnostics.Process process in processes)
+                        {
+                            Win32API.ShowWindowAsync(process.MainWindowHandle, Win32API.SW_SHOWNOMAL);
+                            Win32API.SetForegroundWindow(process.MainWindowHandle);
+                            Win32API.SwitchToThisWindow(process.MainWindowHandle, true);
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    exception.ToString();
+                    // Logger.Error(exception, "唤起已启动进程时出错");
+                }
+
+                //Application.Current.Shutdown();
+                //Environment.Exit(-1);
+                return null;
+            }
+
+            return ProgramStarted;
+        }
+
+    }
+}
--- WPF-Core-Framework/Launcher/App.xaml.cs
+++ WPF-Core-Framework/Launcher/App.xaml.cs
@@ -17,43 +17,35 @@ namespace Launcher
     /// </summary>
     public partial class App : Application
     {
-        public System.Threading.EventWaitHandle ProgramStarted { get; set; }
-
-        private const int SW_SHOWNOMAL = 1;
-
-        ///<summary>
-        /// 该函数设置由不同线程产生的窗口的显示状态
+        /// <summary>
+        /// 程序启动事件
         /// </summary>
-        /// <param name="hWnd">窗口句柄</param>
-        /// <param name="cmdShow">指定窗口如何显示。查看允许值列表，请查阅ShowWindow函数的说明部分</param>
-        /// <returns>如果函数原来可见，返回值为非零；如果函数原来被隐藏，返回值为零</returns>
-        [System.Runtime.InteropServices.DllImport("User32.dll")]
-        private static extern bool ShowWindowAsync(IntPtr hWnd, int cmdShow);
+        public System.Threading.EventWaitHandle ProgramStarted { get; set; }
+        /// <summary>
+        /// 程序唯一标识
+        /// </summary>
+        string mutexName = "78A97384-F4CB-4FBE-990B-8242DEF6AC45";
 
         /// <summary>
-        ///  该函数将创建指定窗口的线程设置到前台，并且激活该窗口。键盘输入转向该窗口，并为用户改各种可视的记号。
-        ///  系统给创建前台窗口的线程分配的权限稍高于其他线程。 
+        /// 构造函数
         /// </summary>
-        /// <param name="hWnd">将被激活并被调入前台的窗口句柄</param>
-        /// <returns>如果窗口设入了前台，返回值为非零；如果窗口未被设入前台，返回值为零</returns>
-        [System.Runtime.InteropServices.DllImport("User32.dll", EntryPoint = "SetForegroundWindow")]
-        private static extern bool SetForegroundWindow(IntPtr hWnd);
-
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        public static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
-
-
         public App()
         {
-            this.InitializeComponent();
-
-            "App初始化完成".WriteToLog(log4net.Core.Level.Info, "AA");
+            this.InitializeComponent(); 
         }
 
+        /// <summary>
+        /// 程序启动
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnStartup(StartupEventArgs e)
         {
-            if (StartOnece())//判断已经启动一次则唤醒程序退出第二次启动。
+            //检查程序是否已经启动过一次
+            ProgramStarted = ApplicationTools.StartOnece(mutexName);
+            if (ProgramStarted == null)//判断已经启动一次则唤醒程序退出第二次启动。
             {
+                Application.Current.Shutdown();
+                Environment.Exit(-1);
                 return;
             }
 
@@ -63,20 +55,8 @@ namespace Launcher
 
             //设置当前程序集
             this.ShutdownMode = ShutdownMode.OnMainWindowClose;
-
-            "App开始启动".WriteToLog(log4net.Core.Level.Info, "BB");
-            base.OnStartup(e);
-            "App完成启动".WriteToLog(log4net.Core.Level.Info, "BB");
-
-            Task.Factory.StartNew(() =>
-            { 
-                do
-                {
-                    "APP task AA 正在写入日志测试！".WriteToLog(log4net.Core.Level.Info, "AA");
-                    "APP task BB 正在写入日志测试！".WriteToLog(log4net.Core.Level.Info, "BB");
-                    System.Threading.Thread.Sleep(1);
-                } while (true); 
-            }); 
+             
+            base.OnStartup(e); 
         }
 
         /// <summary>
@@ -93,8 +73,7 @@ namespace Launcher
             }
             catch (Exception ex)
             {
-                ("不可恢复的异步处理县城异常：《" + ex.ToString() + "》").WriteToLog(log4net.Core.Level.Error);
-                //MessageBox.Show("应用程序发生不可恢复的异常，将要退出！");
+                ("不可恢复的异步处理县城异常：《" + ex.ToString() + "》").WriteToLog(log4net.Core.Level.Error); 
             }
         }
 
@@ -157,47 +136,6 @@ namespace Launcher
         {
             ("当用户结束时发生 Windows 通过注销或关闭操作系统的会话。").WriteToLog(log4net.Core.Level.Info);
             base.OnSessionEnding(e);
-        }
-
-        /// <summary>
-        /// 只打开一个进程
-        /// </summary>
-        protected bool StartOnece()
-        {
-            string mutexName = "32283F61-EC4D-43B1-9C44-40280D5854DD";
-
-            ProgramStarted = new System.Threading.EventWaitHandle(false, System.Threading.EventResetMode.AutoReset, mutexName, out var createNew);
-
-            if (!createNew)
-            {
-                try
-                {
-                    var processes = System.Diagnostics.Process.GetProcessesByName(WorkPath.AssemblyName);
-                    if (!processes.Any())
-                    {
-                        MessageBox.Show("已经启动了XXX", "错误提示", MessageBoxButton.OK, MessageBoxImage.Error);
-                    }
-                    else
-                    {
-                        foreach (System.Diagnostics.Process process in processes)
-                        {
-                            ShowWindowAsync(process.MainWindowHandle, SW_SHOWNOMAL);
-                            SetForegroundWindow(process.MainWindowHandle);
-                            SwitchToThisWindow(process.MainWindowHandle, true);
-                        }
-                    }
-                }
-                catch (Exception exception)
-                {
-                    exception.ToString();
-                    // Logger.Error(exception, "唤起已启动进程时出错");
-                }
-
-                App.Current.Shutdown();
-                Environment.Exit(-1);
-                return true;
-            }
-            return false;
         }
 
     }
